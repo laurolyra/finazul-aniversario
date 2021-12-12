d9b45646b51f96369a55c056b3b288ff9682c2ad
lauro <laurolyra@gmail.com>
2021-12-12 11:20:42 -0300

    concept component ok

--- src/components/Concept/index.js
+++ src/components/Concept/index.js
@@ -1,26 +1,33 @@
 import React from "react";
 import "./styles.css";
+import OrangeBox from "../common/OrangeBox";
 
 const Concept = () => (
   <div className="concept-container mt-3">
-    <div className="container">
+    <div className="container position-relative">
       <div className="row">
-      <h2>O que é o empréstimo?</h2>
-      <h2>Quem pode contratar?</h2>
+        <h2>O que é o empréstimo?</h2>
+        <h2>Quem pode contratar?</h2>
       </div>
-      <div className="pt-3 row justify-content-end text-start mt-2">
-        <div className="col-7 d-flex flex-column">
-          <h3>
-            Modalidade de empréstimo, autorizado pela lei 13.313/16, destinado a
-            trabalhador com carteira assinada (CLT) com idade a partir de 18
-            anos e saldo no FGTS desde que tenha optado pela modalidade
-            saque-aniversário.
-          </h3>
-          <h3>
-            É possível antecipar até 5 parcelas anuais do seu saque-aniversário
-            sendo o saldo mínimo do FGTS de apenas R$ 300,00 para você poder
-            contratar.
-          </h3>
+      <div className="pt-3 position-absolute justify-content-end text-start mt-2 explanation">
+        <div className="d-flex flex-column justify-content-around text-start">
+          <div>
+            <h3>
+              Modalidade de empréstimo, autorizado pela lei 13.313/16, destinado
+              a {" "}
+              <strong>
+                trabalhador com carteira assinada (CLT) com idade a partir de 18
+                anos e saldo no FGTS
+              </strong>{" "}
+              desde que tenha optado pela modalidade saque-aniversário.
+            </h3>
+            <h3>
+              É possível antecipar até 5 parcelas anuais do seu
+              saque-aniversário sendo o saldo mínimo do FGTS de apenas R$ 300,00
+              para você poder contratar.
+            </h3>
+          </div>
+          <OrangeBox />
         </div>
       </div>
     </div>
